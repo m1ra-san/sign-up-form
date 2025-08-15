d6eccdd983f5584ec71939a78b383b07d7296d0a
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-08-15 21:52:18 +0800

    Switch form layout from flex to grid
    Also added info on README about features and technologies used

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# sign-up-form
+# Simple Sign-up Form
+
+## Features
+- Confirm Password functionality
+- Basic form validation
+
+## Technologies & Concepts Learned
+
+**HTML & Forms**  
+- Input types: text, email, number, password  
+- Buttons: submit & reset  
+
+**CSS**  
+- Variables: `var(--font-size)` and `:root`  
+- Functions: `min()`, `max()`, `clamp()`, `calc()`  
+- Positioning: `static`, `relative`, `absolute`  
+- Pseudo-elements/classes  
+- Units: absolute and relative (`em`, `rem`)  
+
+**Validation**  
+- Basic HTML validation & regex patterns  
+
+**Productivity**  
+- Emmet shortcuts
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const submitbtn=document.querySelector("#submit")
+const password=document.querySelector("#password")
+const confirmpassword=document.querySelector("#confirm-password")
+const validationMessage= document.querySelector(".output")
+const fname=document.querySelector("#first-name")
+const lastname=document.querySelector("#last-name")
+
+
 submitbtn.addEventListener("click",(e)=>{
     if(password.value!==confirmpassword.value){
         validationMessage.textContent="Password is not match"
@@ -8,12 +16,7 @@ submitbtn.addEventListener("click",(e)=>{
 });
 
 
-const submitbtn=document.querySelector("#submit")
-const password=document.querySelector("#password")
-const confirmpassword=document.querySelector("#confirm-password")
-const validationMessage= document.querySelector(".output")
-const fname=document.querySelector("#first-name")
-const lastname=document.querySelector("#last-name")
+
 
 
 
